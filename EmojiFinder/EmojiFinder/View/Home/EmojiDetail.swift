6dc141b7f7cdad5a47632219904923525bebb148
Jess <jessen2323@gmail.com>
2024-04-17 11:57:38 +0700

    Redirect dan redacted

---
+++ EmojiFinder/EmojiFinder/View/Home/EmojiDetail.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiDetail.swift
+//  EmojiFinder
+//
+//  Created by User50 on 17/04/24.
+//
+
+import SwiftUI
+
+struct EmojiDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiDetail()
+}
