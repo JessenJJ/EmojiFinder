c0b970d75e3caf00fd6f125a4522c04258da4e64
Jess <jessen2323@gmail.com>
2024-04-17 09:05:07 +0700

    Sinkronisasi git

---
+++ EmojiFinder/EmojiFinder/App/EmojiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiFinderApp.swift
+//  EmojiFinder
+//
+//  Created by User50 on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
