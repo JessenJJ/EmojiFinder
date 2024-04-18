a9ebb1eaebd424f878f3a64a539d5f79f2d46b61
Jess <jessen2323@gmail.com>
2024-04-18 09:25:57 +0700

    Membuat menu

---
+++ EmojiFinder/EmojiFinder/View/Component/InfoBox.swift
@@ -0,0 +1,18 @@
+//
+//  InfoBox.swift
+//  EmojiFinder
+//
+//  Created by User50 on 18/04/24.
+//
+
+import SwiftUI
+
+struct InfoBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoBox()
+}
---
+++ EmojiFinder/EmojiFinder/View/Tab/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  EmojiFinder
+//
+//  Created by User50 on 18/04/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
